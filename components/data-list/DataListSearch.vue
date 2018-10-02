6b3443f51fbc7898565b3ad1818efdb88dc0f238
Daniel <daniel@dev.la>
2018-10-02 20:56:57 +0200

    highlight filter icon

--- components/data-list/DataListSearch.vue
+++ components/data-list/DataListSearch.vue
@@ -14,7 +14,7 @@
 
         <pa-dropdown-item v-if="filters.length">
             <button class="pa-btn pa-btn-white ml-2 h-full focus:shadow-none">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fill-current text-grey-dark h-3 mr-0 md:mr-1"><path d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fill-current h-3 mr-0 md:mr-1" :class="[hasFiltersApplied ? 'text-green' : 'text-grey-dark']"><path d="M487.976 0H24.028C2.71 0-8.047 25.866 7.058 40.971L192 225.941V432c0 7.831 3.821 15.17 10.237 19.662l80 55.98C298.02 518.69 320 507.493 320 487.98V225.941l184.947-184.97C520.021 25.896 509.338 0 487.976 0z"></path></svg>
                 <span class="hidden md:inline-block">Filter</span>
             </button>
 
@@ -47,6 +47,10 @@ export default {
             this.filterStatuses.forEach(({param, active}) => filters[param] = active);
 
             return filters;
+        },
+
+        hasFiltersApplied() {
+            return Object.keys(this.filteredQuery).some(filter => this.filteredQuery[filter] === 1);
         }
     },
 
