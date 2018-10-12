f4750d1c49ceeb1b33b2d4960ec00ed93cd858f5
Daniel <daniel@dev.la>
2018-10-12 14:54:45 +0200

    init spinner

---
+++ components/spinner/Spinner.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <transition name="fade" enter-active-class="duration-0-4 animated fadeIn" leave-active-class="duration-0-4 animated fadeOut">
+            <div class="text-center text-grey">
+                <div class="pa-spinner inline-flex">
+                    <div class="pa-spinner-circle1"></div>
+                    <div class="pa-spinner-circle2 ml-1"></div>
+                    <div class="pa-spinner-circle3 ml-1"></div>
+                </div>
+
+                <p>{{ loadingText }}</p>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['loadingText'],
+}
+</script>
+
+<style></style>
